2dadd4d8395e257d33b4325cedc39ec2e7b991d7
IAmSilK <iamsilk3495@gmail.com>
2017-07-03 12:11:32 -0300

    Added weapon damage and damage multipliers. Sorted/Removed usings.

--- ItemModifier.cs
+++ ItemModifier.cs
@@ -2,10 +2,7 @@
 using Rocket.Core.Plugins;
 using SDG.Unturned;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace ItemModifier
 {
@@ -28,7 +25,7 @@ namespace ItemModifier
 
             Log("Item Modifier by SilK");
             Log("Loading item modifications...");
-            
+
             Type type = typeof(ItemBagAsset);
             Fields.Width = type.GetField("_width", BindingFlags.NonPublic | BindingFlags.Instance);
             Fields.Height = type.GetField("_height", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -50,27 +47,27 @@ namespace ItemModifier
             ItemAsset asset = (ItemAsset)Assets.find(EAssetType.ITEM, modification.ID);
             if (asset == null)
             {
-                LogError("Error: Item ID {0} is invalid.", modification.ID);
+                LogError("Item ID {0} is invalid.", modification.ID);
                 return;
             }
 
             if (asset.isPro)
             {
-                LogError("Error: Item ID {0} is a cosmetic.", modification.ID);
+                LogError("Item ID {0} is a cosmetic.", modification.ID);
                 return;
             }
 
             if (modification.Height.HasValue || modification.Width.HasValue)
             {
-                ItemBagAsset bagAsset = asset as ItemBagAsset;
-                if (bagAsset == null)
+                if (asset is ItemBagAsset)
                 {
-                    LogError("Error: Item ID {0} has no width/height settings.", modification.ID);
+                    ItemBagAsset bagAsset = asset as ItemBagAsset;
+                    if (modification.Width.HasValue) SetWidth(bagAsset, modification.Width.Value);
+                    if (modification.Height.HasValue) SetHeight(bagAsset, modification.Height.Value);
                 }
                 else
                 {
-                    if (modification.Width.HasValue) SetWidth(bagAsset, modification.Width.Value);
-                    if (modification.Height.HasValue) SetHeight(bagAsset, modification.Height.Value);
+                    LogError("Item ID {0} isn't a clothing item with storage.", modification.ID);
                 }
             }
 
@@ -86,16 +83,66 @@ namespace ItemModifier
                 }
                 else
                 {
-                    LogError("Error: Item ID {0} has no health setting.");
+                    LogError("Item ID {0} isn't a structure or barricade.", modification.ID);
+                }
+            }
+
+            if (modification.PlayerDamage.HasValue ||
+                modification.PlayerLegMultipler.HasValue || 
+                modification.PlayerArmMultipler.HasValue ||
+                modification.PlayerSpineMultipler.HasValue ||
+                modification.PlayerSkullMultipler.HasValue ||
+                modification.ZombieDamage.HasValue ||
+                modification.ZombieLegMultipler.HasValue ||
+                modification.ZombieArmMultipler.HasValue ||
+                modification.ZombieSpineMultipler.HasValue ||
+                modification.ZombieSkullMultipler.HasValue ||
+                modification.AnimalDamage.HasValue ||
+                modification.AnimalLegMultipler.HasValue ||
+                modification.AnimalSpineMultipler.HasValue ||
+                modification.AnimalSkullMultipler.HasValue ||
+                modification.BarricadeDamage.HasValue ||
+                modification.StructureDamage.HasValue ||
+                modification.VehicleDamage.HasValue || 
+                modification.ResourceDamage.HasValue ||
+                modification.ObjectDamage.HasValue) // Isn't this pretty?
+            {
+                if (asset is ItemWeaponAsset)
+                {
+                    ItemWeaponAsset weaponAsset = asset as ItemWeaponAsset;
+                    if (modification.PlayerDamage.HasValue) SetPlayerDamage(weaponAsset, modification.PlayerDamage.Value);
+                    if (modification.PlayerLegMultipler.HasValue) SetPlayerLegMultiplier(weaponAsset, modification.PlayerLegMultipler.Value);
+                    if (modification.PlayerArmMultipler.HasValue) SetPlayerArmMultiplier(weaponAsset, modification.PlayerArmMultipler.Value);
+                    if (modification.PlayerSpineMultipler.HasValue) SetPlayerSpineMultiplier(weaponAsset, modification.PlayerSpineMultipler.Value);
+                    if (modification.PlayerSkullMultipler.HasValue) SetPlayerSkullMultiplier(weaponAsset, modification.PlayerSkullMultipler.Value);
+                    if (modification.ZombieDamage.HasValue) SetZombieDamage(weaponAsset, modification.ZombieDamage.Value);
+                    if (modification.ZombieLegMultipler.HasValue) SetZombieLegMultiplier(weaponAsset, modification.ZombieLegMultipler.Value);
+                    if (modification.ZombieArmMultipler.HasValue) SetZombieArmMultiplier(weaponAsset, modification.ZombieArmMultipler.Value);
+                    if (modification.ZombieSpineMultipler.HasValue) SetZombieSpineMultiplier(weaponAsset, modification.ZombieSpineMultipler.Value);
+                    if (modification.ZombieSkullMultipler.HasValue) SetZombieSkullMultiplier(weaponAsset, modification.ZombieSkullMultipler.Value);
+                    if (modification.AnimalDamage.HasValue) SetAnimalDamage(weaponAsset, modification.AnimalDamage.Value);
+                    if (modification.AnimalLegMultipler.HasValue) SetAnimalLegMultiplier(weaponAsset, modification.AnimalLegMultipler.Value);
+                    if (modification.AnimalSpineMultipler.HasValue) SetAnimalSpineMultiplier(weaponAsset, modification.AnimalSpineMultipler.Value);
+                    if (modification.AnimalSkullMultipler.HasValue) SetAnimalSkullMultiplier(weaponAsset, modification.AnimalSkullMultipler.Value);
+                    if (modification.BarricadeDamage.HasValue) SetBarricadeDamage(weaponAsset, modification.BarricadeDamage.Value);
+                    if (modification.StructureDamage.HasValue) SetStructureDamage(weaponAsset, modification.StructureDamage.Value);
+                    if (modification.VehicleDamage.HasValue) SetVehicleDamage(weaponAsset, modification.VehicleDamage.Value);
+                    if (modification.ResourceDamage.HasValue) SetResourceDamage(weaponAsset, modification.ResourceDamage.Value);
+                    if (modification.ObjectDamage.HasValue) SetObjectDamage(weaponAsset, modification.ObjectDamage.Value);
+                }
+                else
+                {
+                    LogError("Item ID {0} isn't a weapon.", modification.ID);
                 }
             }
         }
 
+        #region Bag
         public static bool SetWidth(ItemBagAsset asset, byte width)
         {
             if (Fields.Width == null)
             {
-                LogError("Error: Setting width of Item ID {0}", asset.id);
+                LogError("Setting width of Item ID {0}", asset.id);
                 return false;
             }
 
@@ -108,7 +155,7 @@ namespace ItemModifier
         {            
             if (Fields.Height == null)
             {
-                LogError("Error: Setting height of Item ID {0}", asset.id);
+                LogError("Setting height of Item ID {0}", asset.id);
                 return false;
             }
 
@@ -116,12 +163,14 @@ namespace ItemModifier
 
             return true;
         }
+        #endregion
 
+        #region Barricade/Structure
         public static bool SetHealth(ItemBarricadeAsset asset, ushort health)
         {
             if (Fields.Barricade_Health == null)
             {
-                LogError("Error: Setting health of Item ID {0}", asset.id);
+                LogError("Setting health of Item ID {0}", asset.id);
                 return false;
             }
 
@@ -134,7 +183,7 @@ namespace ItemModifier
         {
             if (Fields.Structure_Health == null)
             {
-                LogError("Error: Setting health of Item ID {0}", asset.id);
+                LogError("Setting health of Item ID {0}", asset.id);
                 return false;
             }
 
@@ -142,8 +191,109 @@ namespace ItemModifier
 
             return true;
         }
+        #endregion
 
-        
+        #region Weapon
+        public static bool SetPlayerDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.playerDamageMultiplier.damage = damage;
+            return true;
+        }
+        public static bool SetPlayerLegMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.playerDamageMultiplier.leg = multiplier;
+            return true;
+        }
+        public static bool SetPlayerArmMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.playerDamageMultiplier.arm = multiplier;
+            return true;
+        }
+        public static bool SetPlayerSpineMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.playerDamageMultiplier.spine = multiplier;
+            return true;
+        }
+        public static bool SetPlayerSkullMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.playerDamageMultiplier.skull = multiplier;
+            return true;
+        }
+
+
+        public static bool SetZombieDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.zombieDamageMultiplier.damage = damage;
+            return true;
+        }
+        public static bool SetZombieLegMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.zombieDamageMultiplier.leg = multiplier;
+            return true;
+        }
+        public static bool SetZombieArmMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.zombieDamageMultiplier.arm = multiplier;  
+            return true;
+        }
+        public static bool SetZombieSpineMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.zombieDamageMultiplier.spine = multiplier;
+            return true;
+        }
+        public static bool SetZombieSkullMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.zombieDamageMultiplier.skull = multiplier;
+            return true;
+        }
+
+        public static bool SetAnimalDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.animalDamageMultiplier.damage = damage;
+            return true;
+        }
+        public static bool SetAnimalLegMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.animalDamageMultiplier.leg = multiplier;
+            return true;
+        }
+        public static bool SetAnimalSpineMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.animalDamageMultiplier.spine = multiplier;
+            return true;
+        }
+        public static bool SetAnimalSkullMultiplier(ItemWeaponAsset asset, float multiplier)
+        {
+            asset.animalDamageMultiplier.skull = multiplier;
+            return true;
+        }
+
+        public static bool SetBarricadeDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.barricadeDamage = damage;
+            return true;
+        }
+        public static bool SetStructureDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.structureDamage = damage;
+            return true;
+        }
+        public static bool SetVehicleDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.vehicleDamage = damage;
+            return true;
+        }
+        public static bool SetResourceDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.resourceDamage = damage;
+            return true;
+        }
+        public static bool SetObjectDamage(ItemWeaponAsset asset, float damage)
+        {
+            asset.objectDamage = damage;
+            return true;
+        }
+        #endregion
 
         internal static void Log(string format, params object[] parameters)
         {
@@ -152,7 +302,7 @@ namespace ItemModifier
 
         internal static void LogError(string format, params object[] parameters)
         {
-            Logger.Log(string.Format(format, parameters), ConsoleColor.Red);
+            Logger.Log("Error: " + string.Format(format, parameters), ConsoleColor.Red);
         }
     }
 }
--- ItemModifierConfiguration.cs
+++ ItemModifierConfiguration.cs
@@ -1,8 +1,5 @@
 ﻿using Rocket.API;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Xml.Serialization;
 
 namespace ItemModifier
@@ -12,10 +9,55 @@ namespace ItemModifier
         [XmlAttribute]
         public ushort ID;
 
+        [XmlElement(IsNullable = false)]
         public byte? Width;
+        [XmlElement(IsNullable = false)]
         public byte? Height;
 
+        [XmlElement(IsNullable = false)]
         public ushort? Health;
+
+        [XmlElement(IsNullable = false)]
+        public float? PlayerDamage;
+        [XmlElement(IsNullable = false)]
+        public float? PlayerLegMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? PlayerArmMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? PlayerSpineMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? PlayerSkullMultipler;
+
+        [XmlElement(IsNullable = false)]
+        public float? ZombieDamage;
+        [XmlElement(IsNullable = false)]
+        public float? ZombieLegMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? ZombieArmMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? ZombieSpineMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? ZombieSkullMultipler;
+
+        [XmlElement(IsNullable = false)]
+        public float? AnimalDamage;
+        [XmlElement(IsNullable = false)]
+        public float? AnimalLegMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? AnimalSpineMultipler;
+        [XmlElement(IsNullable = false)]
+        public float? AnimalSkullMultipler;
+
+        [XmlElement(IsNullable = false)]
+        public float? BarricadeDamage;
+        [XmlElement(IsNullable = false)]
+        public float? StructureDamage;
+        [XmlElement(IsNullable = false)]
+        public float? VehicleDamage;
+        [XmlElement(IsNullable = false)]
+        public float? ResourceDamage;
+        [XmlElement(IsNullable = false)]
+        public float? ObjectDamage;
     }
 
     public class ItemModifierConfiguration : IRocketPluginConfiguration
